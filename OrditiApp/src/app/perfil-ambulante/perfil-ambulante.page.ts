4a46bf9162627534e5276c1a98ee31b98b039521
filipemota130 <45741228+filipemota130@users.noreply.github.com>
2019-11-23 22:24:59 -0300

    perfil ambulante
    modifiquei para apenas labels que recebem informações dos ambulantes apartir do banco de dados

--- OrditiApp/src/app/perfil-ambulante/perfil-ambulante.page.ts
+++ OrditiApp/src/app/perfil-ambulante/perfil-ambulante.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertasService } from '../services/alertas.service';
-import { ModalController } from '@ionic/angular';
-import { MapaModalPage } from '../mapa-modal/mapa-modal.page';
+import { NavParams, ModalController } from '@ionic/angular';
 import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
@@ -11,32 +10,16 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class PerfilAmbulantePage implements OnInit {
 
-  public imgPessoa;
-  public ambulante;
+  ambulante: any;
   // Variaveis da pessoa
-  public nome: string;
-  public cpf: string;
-  public escolaridade: string;
-  public endereco: string;
-  public fone: string;
-
-
-  // Variaveis Do trabalho
-  public produto: string;
-  public pontoRef: string;
-  public localAtiv: string = "Anaaaaaa";
-  public regiao: string;
-
-  // Ponto no MApa
-  public static local: any = { Rua: "Ana", Cep: "Amelia" };
-  static localJson: any;
   constructor(
     public db: AngularFirestore,
     public alertas: AlertasService,
     public modalController: ModalController
     ){ 
-      this.ambulante= db.collection("ambulantes").doc();
+      this.ambulante= NavParams.get("");
   }
+  
   ngOnInit() {
     
 
