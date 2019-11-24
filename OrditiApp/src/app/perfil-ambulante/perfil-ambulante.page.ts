263142e66284de4e1e3608313e5151b4f62c715e
filipemota130 <45741228+filipemota130@users.noreply.github.com>
2019-11-23 22:10:24 -0300

    perfil ambulante

--- OrditiApp/src/app/perfil-ambulante/perfil-ambulante.page.ts
+++ OrditiApp/src/app/perfil-ambulante/perfil-ambulante.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertasService } from '../services/alertas.service';
+import { ModalController } from '@ionic/angular';
+import { MapaModalPage } from '../mapa-modal/mapa-modal.page';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-perfil-ambulante',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilAmbulantePage implements OnInit {
 
-  constructor() { }
+  public imgPessoa;
+  public ambulante;
+  // Variaveis da pessoa
+  public nome: string;
+  public cpf: string;
+  public escolaridade: string;
+  public endereco: string;
+  public fone: string;
 
+
+  // Variaveis Do trabalho
+  public produto: string;
+  public pontoRef: string;
+  public localAtiv: string = "Anaaaaaa";
+  public regiao: string;
+
+  // Ponto no MApa
+  public static local: any = { Rua: "Ana", Cep: "Amelia" };
+  static localJson: any;
+  constructor(
+    public db: AngularFirestore,
+    public alertas: AlertasService,
+    public modalController: ModalController
+    ){ 
+      this.ambulante= db.collection("ambulantes").doc();
+  }
   ngOnInit() {
+    
+
   }
 
 }
