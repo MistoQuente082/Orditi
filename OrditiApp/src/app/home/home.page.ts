094f40994ea0b57061229aad400b7116318fc8b2
Ian Jairo <ijtg.ian@gmail.com>
2019-11-25 12:24:50 -0300

    Modificações

--- OrditiApp/src/app/home/home.page.ts
+++ OrditiApp/src/app/home/home.page.ts
@@ -68,8 +68,11 @@ export class HomePage {
     
     this.qrScanner.prepare()
       .then((status: QRScannerStatus) => {
+<<<<<<< Updated upstream
         if (status.authorized) {
           console.log('check 1')
+=======
+>>>>>>> Stashed changes
           // camera permission was granted
 
 
@@ -81,14 +84,6 @@ export class HomePage {
             this.qrScanner.hide(); // hide camera preview
             scanSub.unsubscribe(); // stop scanning
           });
-
-        } else if (status.denied) {
-          // camera permission was permanently denied
-          // you must use QRScanner.openSettings() method to guide the user to the settings page
-          // then they can grant the permission from there
-        } else {
-          // permission was denied, but not permanently. You can ask for permission again at a later time.
-        }
       })
       .catch((e: any) => console.log('Error is', e));
   }
