494c81d0053519b58acb6d6591a602e2ec020e6d
filipemota130 <45741228+filipemota130@users.noreply.github.com>
2019-11-25 08:54:03 -0300

    perfil + editar ambulatne parte 1

--- OrditiApp/src/app/perfil-ambulante/perfil-ambulante.page.ts
+++ OrditiApp/src/app/perfil-ambulante/perfil-ambulante.page.ts
@@ -2,6 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { AlertasService } from '../services/alertas.service';
 import { NavParams, ModalController } from '@ionic/angular';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { MapaModalPage } from '../mapa-modal/mapa-modal.page';
+import { Observable } from 'rxjs';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+import { AlertController } from '@ionic/angular';
+
+import { Map, latLng, tileLayer, Layer, marker, circle, Icon } from 'leaflet';
+import { Router } from '@angular/router';
+
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import * as firebase from 'firebase'
+import { CameraService } from '../services/camera/camera.service';
 
 @Component({
   selector: 'app-perfil-ambulante',
@@ -20,9 +33,6 @@ export class PerfilAmbulantePage implements OnInit {
   ) {
     // this.ambulante = NavParams.get('item');
   }
-  ngOnInit() {
-
-
-  }
+  ngOnInit() {}
 
 }
